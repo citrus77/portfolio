d3b4ac810ff0419ab26ee3d4337dca1a4b3be0ca
citrus77 <citrus.delicious@outlook.com>
2023-11-11 17:28:10 -0800

    imported data.js from old project

---
+++ src/components/data.js
@@ -0,0 +1,147 @@
+//Skills
+const skills = [
+    {
+        id: 1,
+        name: 'HTML',
+        img: './img/html.png'
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        img: './img/css.png'
+    },
+    {
+        id: 3,
+        name: 'JavaScript',
+        img: './img/js.png'
+    },
+    {
+        id: 4,
+        name: 'Node.js',
+        img: './img/node.png'
+    },
+    {
+        id: 5,
+        name: 'React',
+        img: './img/react.png'
+    },
+    {
+        id: 6,
+        name: 'PostgreSQL',
+        img: './img/elephant.png'
+    },
+    {
+        id: 7,
+        name: 'Playwright',
+        img: './img/playwright.svg'
+    }
+]
+
+const getSkills = () => {
+    return skills;
+};
+
+//Portfolio
+const projects = [
+    {
+        id: 1,
+        project: 'Connect-Four',
+        img: './img/screenshots/connectfour.png',
+        url: 'https://elegant-jones-1aa5f1.netlify.app/',
+        github: 'https://github.com/citrus77/connect-four',
+        contributors: [
+            {
+                id: 1,
+                name: 'Jason Lammers',
+                github: 'https://github.com/citrus77'
+            }
+        ],
+        description: `One of my earlier projects, this recreation of the classic game Connect-Four! solidified my understanding of JavaScript.`
+    },
+    {
+        id: 2,
+        project: `Stranger's Things`,
+        img: './img/screenshots/strangersthings.png',
+        url: 'https://stoic-raman-95405a.netlify.app/',
+        github: 'https://github.com/citrus77/Strangers.Things.Project.git',
+        contributors: [
+            {
+                id: 1,
+                name: 'Jason Lammers',
+                github: 'https://github.com/citrus77'
+            }
+        ],
+        description: `This solo frontend project served as my introduction to React.js. It was built with a pre-made API and is intended to be somewhat of a Craig's List clone.`
+    },
+    {
+        id: 3,
+        project: `Awesome Shoe Store`,
+        img: './img/screenshots/awesomeshoestore.png',
+        url: 'https://joyful-nodes.herokuapp.com/',
+        github: 'https://github.com/2105-SJS/joyful-nodes',
+        contributors: [
+            {
+                id: 1,
+                name: 'Jason Lammers',
+                github: 'https://github.com/citrus77'
+            },
+            {
+                id: 2,
+                name: 'Carlos Escamilla',
+                github: 'https://github.com/carlos-escamilla33'
+            },
+            {
+                id: 3,
+                name: 'Juan Soto',
+                github: 'https://github.com/juansotojs'
+            }
+        ],
+        description: 'An ecommerce site built with React.js, Node.js and PostgreSQL. This fullstack project was completed in an agile environment with a team of 3. This was my first real experience collaborating with other people on the same project.'
+    },
+];
+
+const getProjects = () => {
+    return projects;
+};
+
+const aboutMe = {
+    name: 'Jason Lammers',
+    email: 'jason.lammers@outlook.com',
+    github: 'https://github.com/citrus77',
+    linkedIn: 'https://www.linkedin.com/in/jason-lammers/',    
+    resumeLnk: 'https://1drv.ms/b/s!ArNExx5K-yt6gYS8QZX117ytuk9xAjU?e=cPVw4w',
+    description: 'Web Developer seeking employment with a dynamic tech team. Known as an analytical, problem-solving professional with strong time-management skills and the ability to learn concepts quickly.  5+ years of experience working directly in customer service and 10+ years working in fast-paced team environments.'
+};
+
+const getAboutMe = () => {
+    return aboutMe;
+};
+
+//Education
+const education = [
+    {
+        id: 1,
+        name: 'Fullstack Academy',
+        location: 'San Jose State University',
+        startMonth: 'May',
+        startYear: '2021',
+        endMonth: 'November',
+        endYear: '2021',
+        description: 'Certificate in Web Development- Powered by Fullstack Academy. Rigorous coding bootcamp focused on fullstack development. Emphasis on paired programming in an agile environment.',
+        logo: './img/hanover-square-logo.png'
+    }
+];
+
+const getEducation = () => {
+    return education;
+};
+
+//Exports
+const dataExports = { 
+    getAboutMe, 
+    getProjects, 
+    getSkills, 
+    getEducation
+};
+
+export default dataExports;
